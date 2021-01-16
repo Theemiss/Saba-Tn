b65e32c5a59e94e6a38c8d1dd0fcdc4f6eb2b1a2
achrefbs <siachrefboularess@gmail.com>
2021-01-16 18:09:57 +0100

    tst db

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from datetime import datetime
+from users.models import User
 # Create your models here.
+
+class BaseListing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200, blank=True) #optional for now should be auto generated
+    description = models.TextField(blank=True) #optional
+    price = models.FloatField()
+    city = models.CharField(max_length=200) #should be choice
+    state = models.CharField(max_length=200) #should be choice
+    category = models.CharField(max_length=200) #should be foreign key I guess
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True) #optional for now
+    add_date = models.DateTimeField(default=datetime.now, blank=True)
+    update_date = models.DateTimeField(default=datetime.now, blank=True)
+    #we should add is_accepted, is_declined and is_waiting (BooleanField)
+    def __str__(self):
+        return self.title
