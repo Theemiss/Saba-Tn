e32d0e8a1b146526c94b7765b89aa7d99f2bcb1a
achrefbs <siachrefboularess@gmail.com>
2021-01-16 16:06:55 +0100

    pages

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'listings.html')
+
+
+def details(request, listing_id):
+    return render(request, 'details.html')
